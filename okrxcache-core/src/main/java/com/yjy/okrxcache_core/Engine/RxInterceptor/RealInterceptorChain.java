744b7e217d9ea4b3c7aaafab113c1241152c4ea8
yjy239 <yujunyu12@gmail.com>
2019-06-21 13:48:22 +0800

    处理拦截器循环

--- okrxcache-core/src/main/java/com/yjy/okrxcache_core/Engine/RxInterceptor/RealInterceptorChain.java
+++ okrxcache-core/src/main/java/com/yjy/okrxcache_core/Engine/RxInterceptor/RealInterceptorChain.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import io.reactivex.Observable;
 
+
 /**
  * <pre>
  *     author : yjy
@@ -49,14 +50,15 @@ public class RealInterceptorChain<T> implements Interceptor.Chain {
         }
         RealInterceptorChain next = new RealInterceptorChain(mInterceptors,mIndex+1,mRequest,mMode);
         Interceptor interceptor = mInterceptors.get(mIndex);
-        interceptor.setMode(mMode);
-        try {
-            mObservale = interceptor.intercept(next);
-        }catch (Exception e){
-            mObservale = Observable.empty();
-            e.printStackTrace();
+        if(interceptor != null){
+            interceptor.setMode(mMode);
+            try {
+                mObservale = interceptor.intercept(next);
+            }catch (Exception e){
+                mObservale = Observable.empty();
+                e.printStackTrace();
+            }
         }
-
         return mObservale;
     }
 }
